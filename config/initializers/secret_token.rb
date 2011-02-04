1e0f89ef7350bbf5fb4c45feb3b3b5c515376a49
Eric D. Fields <ericdfields@gmail.com>
2011-02-03 22:17:07 -0500

    new rails app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Goodspot::Application.config.secret_token = 'f96be1bf7616f64df2556516a7c8601dc3c44f1db73c8f9c7f0f348438aa35a88f00672410caf6a69286426625e3f085fb143d0a7decfabd7dc692d5e90904c5'
